d86e23552586b36606839638988520349b5a5f62
hsck98 <hsck98@gmail.com>
2023-05-31 19:38:42 +0100

    Project Files

---
+++ book_resources/ResetPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPlayer : MonoBehaviour
+{
+    Vector3 startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y <= -5) transform.position = startPosition;
+    }
+}
